9142f5b60e864ebb300f226391e0734ff8a9ff10
AndresDev711 <andres7condori@gmail.com>
2023-09-03 13:55:37 -0500

    update

--- src/features/controllers/index.controller.ts
+++ src/features/controllers/index.controller.ts
@@ -4,7 +4,3 @@ export const indexWelcome = (_req: Request, res: Response) => {
     return res.json('WELCOME TO MY API')
 }
 
-export const sum(num: Number, number2: Number) => {
-    return res.json(num + number2)
-}
-		
